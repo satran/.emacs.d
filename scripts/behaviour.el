37cea921c3596690695af9706f595fc9d8726d48
Satyajit Ranjeev <s@ranjeev.in>
2016-10-18 22:08:04 +0200

    Configurations for backups and history

--- scripts/behaviour.el
+++ scripts/behaviour.el
@@ -23,6 +23,12 @@
 ;;(setq linum-format " %d ")
 ;;(global-linum-mode t)
 
+;; Do not pollute the working directory. Add it to emacs folder.
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 
 ;; split frame into 3 windows evenly
 (defun split-3-even ()
@@ -30,3 +36,27 @@
   (dotimes (number 2)
     (split-window-right))
   (balance-windows))
+
+
+;; save history
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
+;; When you start typing and text is selected, replace it with what
+;; you are typing, or pasting, or whatever.
+(delete-selection-mode 1)
+
+;; Change typing yes to y and no to n in minibuffer
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; powerline to setup mode line
+(setq powerline-display-buffer-size nil)
+(setq powerline-display-hud nil)
+(powerline-default-theme)
